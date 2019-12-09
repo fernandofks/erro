17351ef925a26801f40896aa0e3d4fe53fa36879
Fernando Kenji Sakabe <fernandokenjisakabe@gmail.com>
2019-12-09 17:19:58 -0300

    Erro de for e duplicação 3

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-for(let i=0; i<10; i--){console.log(i)}
-=======
 for(let j=0; j<10; j--){console.log(j)}
 
 function soma1(a, b){
@@ -11,4 +8,3 @@ function soma2(a, b){
     return a+b
 }
 
->>>>>>> 096009685d7d16e276d182446abeda20fff6c7b8
